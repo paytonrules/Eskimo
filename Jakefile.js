d755ed15d79904985fb38150d32bfb4648ee7fdc
Eric Smith <eric@8thlight.com>
2011-08-28 08:10:23 -0500

    Added a Jakefile, can now run specs and minify. Make sure you include the closure compiler and ignore the minified version.

---
+++ Jakefile.js
@@ -0,0 +1,40 @@
+function sh(command) {
+  var sys = require('sys')
+  var exec = require('child_process').exec;
+  function puts(error, stdout, stderr) { sys.puts(stdout); }
+  exec(command, puts);
+}
+
+desc('Run Specs')
+task("spec", function() {
+  sh('jessie spec');
+});
+
+desc("Install dependencies")
+task("dependencies", function() {
+  sh('npm install glob');
+  sh('npm install html5');
+  sh('npm install jessie -g');
+  sh('npm install jquery');
+  sh('npm install jsdom');
+  sh('npm install underscore');
+});
+
+desc('Build the project')
+task("build", function() {
+  var glob = require('glob');
+  var _ = require('underscore'); 
+  var files = glob.globSync("./src/*.js");
+
+  command_string = "java -jar compiler.jar --js=node_modules/underscore/underscore-min.js";
+  command_string += " --js=./src/main.js"; // Main needs to come first
+
+  files = _.reject(files, function(fileName) { return fileName.indexOf("main.js") != -1 } );
+  _(files).each(function(file) {
+    command_string += " --js=" + file;
+  });
+  command_string += " --js_output_file=eskimo.min.js"
+  console.log(command_string);
+
+  sh(command_string);
+});
