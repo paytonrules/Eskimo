ab535c4e755e221cb3e0bc790110c82b1224d6ea
Eric Smith <eric@8thlight.com>
2011-09-02 08:10:50 -0500

    Name the sounds - jukebox

--- spec/spec_helper.js
+++ spec/spec_helper.js
@@ -3,7 +3,7 @@ require("../src/assets");
 require("../src/fixed-game-loop");
 require("../src/scheduler");
 require("../src/screen");
-require("../src/sound");
+require("../src/jukebox");
 require("../src/vector");
 
 _ = require("underscore");
