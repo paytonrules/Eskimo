098f72700409703ca93536c8e87a1b3b254be69f
Eric Smith <eric@8thlight.com>
2012-01-26 21:53:36 -0600

    Export Screen, for testing

--- src/index.js
+++ src/index.js
@@ -3,3 +3,4 @@ module.exports.Level = require("./level");
 module.exports.Image = require("./image");
 module.exports.Jukebox = require("./jukebox");
 module.exports.KeyCodes = require("./key-codes");
+module.exports.Screen = require("./screen");
