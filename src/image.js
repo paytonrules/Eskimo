7c137a0109f886e45094a7b7c4f4fa9167462539
Eric Smith <eric@8thlight.com>
2012-01-26 21:25:14 -0600

    Remove some silly commented code

--- src/image.js
+++ src/image.js
@@ -4,29 +4,14 @@ module.exports = function(gameObject) {
   var name = _(keys).first();
   var properties = gameObject[name];
 
-/*
- *  function right() {
- *    return image().width + x;
- *  }
- *
- *  function bottom() {
- *    return image().height + y;
- *  }
- *
- */
   function draw(context) {
-    context.drawImage(properties.asset, properties.location.x, properties.location.y);
+    context.drawImage(properties.asset, 
+                      properties.location.x, 
+                      properties.location.y);
   }
 
   return {
     name: name,
-    /*
-     *location: location,
-     *x: location.x,
-     *y: location.y,
-     *right: right,
-     *bottom: bottom,
-     */
     draw: draw
   };
 };
