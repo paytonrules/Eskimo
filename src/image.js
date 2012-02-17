1d66485ac0d6668f35d5a7a29abdd0126648614d
Eric Smith <eric@8thlight.com>
2012-02-16 22:16:54 -0600

    Wrote a simple contains function for the image, for detecting the click

--- src/image.js
+++ src/image.js
@@ -7,8 +7,33 @@ module.exports = function(name, gameObject) {
                       gameObject.location.y);
   }
 
+  function right() {
+    return gameObject.location.x + gameObject.asset.width;
+  }
+
+  function bottom() {
+    return gameObject.location.y + gameObject.asset.height;
+  }
+
+  function withinXBounds(x) {
+    return (x >= gameObject.location.x && x <= right());
+  }
+
+  function withinYBounds(y) {
+    return (y >= gameObject.location.y && y <= bottom());
+  }
+
+  function contains(x, y) {
+    if (withinXBounds(x) && withinYBounds(y)) {
+      return true;
+    }
+
+    return false;
+  }
+
   return {
     name: name,
-    draw: draw
+    draw: draw,
+    contains: contains
   };
 };
--- test/image_spec.js
+++ test/image_spec.js
@@ -33,5 +33,22 @@ describe("Image", function() {
     canvasSpy.calledWith('asset', 10, 20).should.be.true;
   });
 
+  it("returns whether a point is inside or outside the bounding box", function() {
+    var jquery = require('jquery');
+    var image = Image("name", {
+                        location: {
+                          x: 1,
+                          y: 1,
+                        },
+                        asset: jquery("<img src='blah' width='20' height='20'>")[0]
+                      });
+
+    image.contains(1, 1).should.be.true;
+    image.contains(0, 1).should.be.false;
+    image.contains(1, 0).should.be.false;
+    image.contains(21, 21).should.be.true;
+    image.contains(22, 21).should.be.false;
+    image.contains(21, 22).should.be.false;
+  });
 
 });
