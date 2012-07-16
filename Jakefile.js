3922888990c5da11195a546df1e1f2c5201da437
Eric Smith <eric@8thlight.com>
2012-07-16 07:44:05 -0500

    Cleanup the Jakefile

--- Jakefile.js
+++ Jakefile.js
@@ -1,41 +1,16 @@
 function sh(command) {
-  var sys = require('sys')
+  var sys = require('util');
   var exec = require('child_process').exec;
   function puts(error, stdout, stderr) { sys.puts(stdout); }
   exec(command, puts);
 }
 
-desc('Run Specs')
+desc('Run Specs');
 task("spec", function() {
-  sh('jessie spec');
+  sh('. run_specs');
 });
 
-desc("Install dependencies")
+desc("Install dependencies");
 task("dependencies", function() {
-  sh('npm install glob');
-  sh('npm install html5');
-  sh('npm install mocha -g');
-  sh('npm install jquery');
-  sh('npm install jsdom');
-  sh('npm install underscore');
-});
-
-desc('Build the project')
-task("build", function() {
-  var glob = require('glob');
-  var _ = require('underscore'); 
-  var files = glob.globSync("./src/*.js");
-
-  command_string = "java -jar compiler.jar --js=node_modules/underscore/underscore-min.js";
-  command_string += " --js=./src/main.js"; // Main needs to come first
-
-  files = _.reject(files, function(fileName) { return fileName.indexOf("main.js") != -1 } );
-  _(files).each(function(file) {
-    command_string += " --js=" + file;
-  });
-  command_string += " --create_source_map=eskimo.map";
-  command_string += " --js_output_file=eskimo.min.js";
-  console.log(command_string);
-
-  sh(command_string);
+  sh('npm install canvas html5 jquery jsdom mocha -g should sinon underscore');
 });
