b28c2cf68b96649b52ed1ff87adb1b0ab6b6ae4b
Eric Smith <paytonrules@gmail.com>
2012-09-14 08:28:20 -0500

    modify key codes

--- src/key-codes.js
+++ src/key-codes.js
@@ -1,11 +1,11 @@
 // Might wanna fill these in
 module.exports = {
-  BACKSPACE: 8,
-  SPACEBAR: 32,
-  LEFT_ARROW: 37,
-  UP_ARROW: 38,
-  RIGHT_ARROW: 39,
-  DOWN_ARROW: 40,
-  DELETE: 46,
-  APOSTROPHE: 222
+  8:   'backspace',
+  32:  'spacebar',
+  37:  'left',
+  38:  'up',
+  39:  'right',
+  40:  'down',
+  46:  'delete',
+  222: 'apostrophe'
 };
