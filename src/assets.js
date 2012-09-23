7ae1f23f386061174b730e5f2da33b1b4fc421d8
Eric Smith <paytonrules@gmail.com>
2012-09-22 20:12:16 -0500

    Move load out of asset.  Now to cleanup asset loader

--- src/assets.js
+++ src/assets.js
@@ -1,47 +1,19 @@
 module.exports = function(options) {
   var assetList = {},
-      jquery = options['jquery'],
-      tag = options['tag'],
-      loadEvent = options['loadEvent'],
-      size = 0;
+      _ = require('underscore');
 
   this.get = function(key) {
-    if ( assetList[key] && assetList[key].loaded ) {
+    if ( assetList[key]) {
       return assetList[key].get(0);
     }
     return null;
   };
 
-  this.load = function(key, src, fn) {
-    if (assetList[key]) {
-      throw {name: "Eskimo.AssetAlreadyExists", message: "Asset '" + src + "' already exists"};
-    } else {
-      var self = this;
-      assetList[key] = jquery("<" + tag + " src='" + src + "'>");
-      assetList[key].bind(loadEvent, function() {
-        if (assetList[key]) {
-          assetList[key].loaded = true;
-          
-          if (typeof(fn) !== "undefined") {
-            fn(self.get(key));
-          } 
-        }
-      });
-    }
-    size++;
-  };
-
   this.add = function(key, obj) {
-    assetList[key] = obj;
-    assetList[key].loaded = true;
+    assetList[key] = obj; 
   };
 
   this.size = function() {
-    return size;
-  };
-
-  this.clear = function() {
-    size = 0;
-    assetList = {};
+    return _.keys(assetList).length;
   };
 };
--- test/assets_spec.js
+++ test/assets_spec.js
@@ -1,96 +1,19 @@
 describe("Assets", function() {
-  var should = require('should'),
-      sinon = require('sinon'),
-      sandbox = sinon.sandbox.create(),
-      Assets = require("../src/assets"),
-      jquerySpy,
-      assets,
-      window;
-
-  function prepareHTML5() {
-    var dom = require('jsdom').jsdom(),
-        define = require('../node_modules/jsdom/lib/jsdom/level2/html').define;
-    window = dom.createWindow();
-
-    require("jquery").create(window);
-
-    define("HTMLAudioElement", {
-      tagName: 'AUDIO',
-      attributes: [
-        'src'
-      ]
-    });
-
-    jquerySpy = sandbox.spy(window, 'jQuery');
-  }
-
-  beforeEach(function() {
-    prepareHTML5();
-
-    assets = new Assets({jquery: window.jQuery, 
-                         tag: 'img',
-                         loadEvent: 'loadEvent'});
-  });
-
-  afterEach(function() {
-    sandbox.restore();
-  });
-  
-  it("doesn't have an asset if it hasn't been loaded yet by the user", function() {
-    should.not.exist(assets.get('key'));
-  });
-
-  it("doesn't get the asset if the image hasn't been loaded into the DOM", function() {
-    assets.load('key', 'src');
-
-    should.not.exist(assets.get('key'));
-  });
-
-  it("can get the asset after it's loadevent is triggered", function() {
-    assets.load('key', 'src');
-    jquerySpy.returnValues[0].trigger('loadEvent');
-
-    var asset = assets.get('key');
-
-    asset.should.be.ok;
-  });
-
-  it("makes an optional callback with the asset when the object is loaded successfully", function() {
-    var callback = sandbox.stub();
-
-    assets.load('key', 'src', callback);
-    jquerySpy.returnValues[0].trigger('loadEvent');
+  var Assert = require('assert'),
+      Assets = require("../src/assets");
+   
+  it("returns null for assets that don't exist", function() {
+    var assets = new Assets();
     
-    callback.calledWith(assets.get('key')).should.be.true;
+    Assert.equal(0, assets.size());
   });
 
-  it("sets up the asset with the passed in source", function() {
-    assets.load('key', 'src');
-    jquerySpy.returnValues[0].trigger('loadEvent');
+  it("returns assets that do exist, matching the jquery format", function() {
+    var assets = new Assets();
 
-    var asset = assets.get('key');
+    assets.add('key', {get: function(index) { return "bleh";}});
 
-    asset.src.should.equal('src');
+    Assert.equal("bleh", assets.get('key'));
   });
 
-  it("gives the asset the tag passed into the constructor", function() {
-    assets.load('key', 'src');
-    jquerySpy.returnValues[0].trigger('loadEvent');
-
-    var asset = assets.get('key');
-
-    asset.tagName.should.equal('IMG');
-  });
-
-  it("raises an exception if there is already an asset with that key", function() {
-    assets.load('key', 'src');
-
-    try {
-      assets.load('key', 'error');
-      should.fail("Should have thrown an exception, but didn't");
-    } catch (err) {
-      err.should.eql({name: "Eskimo.AssetAlreadyExists",
-                      message:"Asset 'error' already exists"});
-    }
-  });
 });
