e9954c88dc3e02c91f8b0017b62f57dd346921f3
paytonrules <esmith@8thlight.com>
2013-01-04 19:45:19 -0600

    Return to using the passed in game object

--- src/image.js
+++ src/image.js
@@ -1,27 +1,25 @@
 module.exports = function(name, gameObject) {
-  var location = {x: gameObject.location ? gameObject.location.x : 0, 
-                 y: gameObject.location ? gameObject.location.y : 0};
 
   function draw(context) {
     context.drawImage(gameObject.asset,  // This will be a DOM object 
-                      location.x, 
-                      location.y);
+                      gameObject.location.x,
+                      gameObject.location.y);
   }
 
   function right() {
-    return location.x + gameObject.asset.width;
+    return gameObject.location.x + gameObject.asset.width;
   }
 
   function bottom() {
-    return location.y + gameObject.asset.height;
+    return gameObject.location.y + gameObject.asset.height;
   }
 
   function withinXBounds(x) {
-    return (x >= location.x && x <= right());
+    return (x >= gameObject.location.x && x <= right());
   }
 
   function withinYBounds(y) {
-    return (y >= location.y && y <= bottom());
+    return (y >= gameObject.location.y && y <= bottom());
   }
 
   function contains(x, y) {
