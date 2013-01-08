b03f5a241c4c2169fbf51829b37af8319e03d7ba
paytonrules <esmith@8thlight.com>
2013-01-08 00:43:11 -0600

    Hack to put height/width on fake assets

--- src/test_helpers/test_asset_loader.js
+++ src/test_helpers/test_asset_loader.js
@@ -20,6 +20,14 @@ module.exports = function(type, completeCallback) {
     _assetLoader.load(level);
     _.each(returnValues, function(returnValue) {
       returnValue.trigger(_assetLoader.getLoadEvent());
+      for(var obj in level) {
+        // NOTE this only works with test objects
+        // Certainly a better way
+        if (level[obj].asset === returnValue[0] && level[obj].testAsset) {
+          returnValue[0].width = level[obj].testAsset.width;
+          returnValue[0].height = level[obj].testAsset.height;
+        }
+      }
     });
   }
 };
