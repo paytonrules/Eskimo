6a15621ac51b4c5d17720ee3b635823b34c1fa9d
paytonrules <esmith@8thlight.com>
2013-01-23 09:06:18 -0600

    Load up sprite sheets

--- src/object_pipeline/register_default_loaders.js
+++ src/object_pipeline/register_default_loaders.js
@@ -1,9 +1,11 @@
 var SpriteLoader = require('./sprite_loader'),
-    SoundLoader = require('./sound_loader');
+    SoundLoader = require('./sound_loader'),
+    SpriteSheetLoader = require('./sprite_sheet_loader');
 
 module.exports = {
   register: function(spec, assetLoader) {
     spec.registerLoader('sprite', SpriteLoader.create(assetLoader));
     spec.registerLoader('sound', SoundLoader.create(assetLoader));
+    spec.registerLoader('sprite_sheet', SpriteSheetLoader.create(assetLoader));
   }
 };
