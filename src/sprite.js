b6e7bc06c059967bfcf89d41738d6b2175c4657f
paytonrules <esmith@8thlight.com>
2013-01-25 09:16:24 -0600

    Rename Image to Sprite

--- src/sprite.js
+++ src/sprite.js
@@ -1,35 +1,35 @@
 var jquery = require('jquery');
 
 module.exports = function(name, gameObject) {
-  var Image = function() {
+  var Sprite = function() {
     jquery.extend(true, this, gameObject);
     jquery.extend(true, this, require('./geometry/rectangle.js'));
     this.name = name;
   };
 
-  Image.prototype.draw = function(context) {
+  Sprite.prototype.draw = function(context) {
     context.drawImage(this.asset,  // This will be a DOM object 
                       this.location.x,
                       this.location.y);
   };
 
-  Image.prototype.right = function() {
+  Sprite.prototype.right = function() {
     return this.location.x + this.asset.width;
   };
 
-  Image.prototype.bottom = function() {
+  Sprite.prototype.bottom = function() {
     return this.location.y + this.asset.height;
   };
 
-  Image.prototype.withinXBounds = function(x) {
+  Sprite.prototype.withinXBounds = function(x) {
     return (x >= this.location.x && x <= this.right());
   };
 
-  Image.prototype.withinYBounds = function(y) {
+  Sprite.prototype.withinYBounds = function(y) {
     return (y >= this.location.y && y <= this.bottom());
   };
 
-  Image.prototype.contains = function(x, y) {
+  Sprite.prototype.contains = function(x, y) {
     if (this.withinXBounds(x) && this.withinYBounds(y)) {
       return true;
     }
@@ -37,29 +37,29 @@ module.exports = function(name, gameObject) {
     return false;
   };
 
-  Image.prototype.width = function() {
+  Sprite.prototype.width = function() {
     return this.asset.width;
   };
 
-  Image.prototype.height = function() {
+  Sprite.prototype.height = function() {
     return this.asset.height;
   };
 
-  Image.prototype.right = function() {
+  Sprite.prototype.right = function() {
     return this.location.x + this.asset.width;
   };
 
-  Image.prototype.left = function() {
+  Sprite.prototype.left = function() {
     return this.location.x;
   };
 
-  Image.prototype.bottom = function() {
+  Sprite.prototype.bottom = function() {
     return this.location.y + this.asset.height;
   };
 
-  Image.prototype.top = function() {
+  Sprite.prototype.top = function() {
     return this.location.y;
   };
 
-  return new Image(gameObject);
+  return new Sprite(gameObject);
 };
